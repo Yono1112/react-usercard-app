fb38b930783eb5419e73b13675633d646cbfab76
Yono1112 <yuma.ohno.sss@gmail.com>
2023-10-08 21:40:14 +0900

    create useMessage hook used to display messages at login

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,26 +3,28 @@ import axios from "axios";
 import { useCallback, useState } from "react"
 import { User } from "../types/api/user";
 import { useNavigate } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
 	const navigate = useNavigate();
 	const [ loading, setLoding ] = useState(false);
+	const { ShowMessage } = useMessage();
 
 	const login = useCallback((id: string) => {
 		if (!id.trim()) {
-			alert("ユーザーIDを入力してください");
+			ShowMessage({ title: "ユーザーIDを入力してください", status: "warning"});
 			return;
 		}
 		setLoding(true);
 		axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
 			if (res.data && res.data.id) {
-				console.log(res.data);
+				ShowMessage({ title: "ログインしました", status: "success"});
 				navigate("/home");
 			} else {
-				alert("ログインできません")
+				ShowMessage({ title: "ログインできません", status: "error"});
 			}
 		})
-		.catch(() => alert("ユーザーが見つかりません"))
+		.catch(() => ShowMessage({ title: "ユーザーが見つかりません", status: "error"}))
 		.finally(() => setLoding(false));
 	}, []);
 	return { login, loading };
