3323f49446c36d98dfe585877e413ed24fc360e5
Yono1112 <yuma.ohno.sss@gmail.com>
2023-10-08 22:55:35 +0900

    display all users requested from API by using useAllUsers hook

--- src/components/pages/UserCardManagement.tsx
+++ src/components/pages/UserCardManagement.tsx
@@ -1,13 +1,32 @@
-import {  Wrap, WrapItem } from "@chakra-ui/react"
-import { FC, memo } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+import {  Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react"
+import { FC, memo, useEffect } from "react"
 import { UserCard } from "../organisms/user/UserCard"
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserCardManagement: FC = memo(() => {
+	const { getUsers, users, loading} = useAllUsers();
+	useEffect(() => getUsers(), []);
+
 	return (
-		<Wrap p={{ base: 4, md: 10}}>
-			<WrapItem>
-				<UserCard imageUrd="https://source.unsplash.com/TaCk3NspYe0" userName="hoge" fullName="hogehoge"/>
-			</WrapItem>
-		</Wrap>
+		<>
+			{loading ? (
+				<Center h="100vh">
+					<Spinner />
+				</Center>
+			): (
+				<Wrap p={{ base: 4, md: 10}}>
+					{users.map((user) => (
+						<WrapItem key={user.id} mx="auto">
+							<UserCard
+								imageUrd="https://source.unsplash.com/TaCk3NspYe0"
+								userName={user.username}
+								fullName={user.name}
+							/>
+						</WrapItem>
+					))}
+				</Wrap>
+			)}
+		</>
 	)
 })
